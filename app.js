333c5ca33205cd4be982390f37c9a0d4ecb225f6
liyingda <liyingda@myhexin.com>
2024-07-16 21:25:03 +0800

    feat: 初始化项目

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
 const index = require('./routes/index')
-const users = require('./routes/users')
+const extension = require('./routes/extension')
 
 // error handler
 onerror(app)
@@ -34,7 +34,7 @@ app.use(async (ctx, next) => {
 
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(extension.routes(), extension.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,24 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
+// http://localhost:3000/get?size=100
+router.get('/get', async (ctx, next) => {
+  const { size } = ctx.query
+  ctx.body = `您的size参数是${size}`
+})
+
+// http://localhost:3000/get/koa
+router.get('/get/:id', async (ctx, next) => {
+  const { id } = ctx.params
+  ctx.body = `您的id参数是${id}`
+})
+
+router.post('/juejin', async (ctx, next) => {
+  const { name, id } = ctx.request.body
+  ctx.body = {
+    name:`您的用户名是${name}`,
+    id: `${id}`,
+  }
+})
+
 module.exports = router
